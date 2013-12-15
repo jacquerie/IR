2e6f3dfd9d372fb52dc83327eb83cec30f78c468
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2013-12-15 03:32:21 +0100

    Add MilneWitten.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         TagmeConfig.init("/home/irproject/config.xml");
         String groupName = "group3";
         String groupPw = "7dGHlSa";
-        RelatednessMeasure rel = new OutJaccard("en");
-        IRProjectExperiments.dumpRelatednessExperiment(groupName, rel);
-        // IRProjectExperiments.launchTagMeExperiment(groupName, groupPw, rel);
-        // IRProjectExperiments.launchRelatednessExperiment(groupName, groupPw, rel);
+        RelatednessMeasure rel = new MilneWitten("en");
+        // IRProjectExperiments.dumpRelatednessExperiment(groupName, rel);
+        IRProjectExperiments.launchTagMeExperiment(groupName, groupPw, rel);
+        IRProjectExperiments.launchRelatednessExperiment(groupName, groupPw, rel);
     }
 
 }
