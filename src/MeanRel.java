5d6d4790e47387d84283584a0e2cba4aecfa5340
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2013-12-15 02:19:47 +0100

    Rename DummyRel in MeanRel.

--- src/MeanRel.java
+++ src/MeanRel.java
@@ -1,14 +1,14 @@
 import it.acubelab.tagme.RelatednessMeasure;
 
-public class DummyRel extends RelatednessMeasure {
+public class MeanRel extends RelatednessMeasure {
 
-    public DummyRel (String lang) {
+    public MeanRel (String lang) {
         super(lang);
     }
 
     @Override
     public float rel (int a, int b) {
-        return 0.0f;
+        return 0.596429f;
     }
 
 }
