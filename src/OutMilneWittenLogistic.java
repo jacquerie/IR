0bed2ba7c0977bea63cb51053a8e989fffdda261
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-01-28 01:38:20 +0100

    Add measure counterparts on outlinks.

---
+++ src/OutMilneWittenLogistic.java
@@ -0,0 +1,49 @@
+import java.lang.Math;
+
+import irproject.IRProjectHelper;
+import it.acubelab.tagme.RelatednessMeasure;
+
+public class OutMilneWittenLogistic extends RelatednessMeasure {
+
+    public OutMilneWittenLogistic (String lang) {
+        super(lang);
+    }
+
+    @Override
+    public float rel (int first, int second) {
+        if (first == second) {
+            return 1.0f;
+        }
+
+        int[] outFirst = IRProjectHelper.getOutlinks(first);
+        int[] outSecond = IRProjectHelper.getOutlinks(second);
+        int wikipediaSize = IRProjectHelper.getWikipediaSize();
+        int intersectionSize = getIntersectionSize(outFirst, outSecond);
+
+        float numerator = (float) Math.log(Math.max(outFirst.length, outSecond.length)) - (float) Math.log(intersectionSize);
+        float denominator = (float) Math.log(wikipediaSize) - (float) Math.log(Math.min(outFirst.length, outSecond.length));
+        float result = numerator / denominator;
+
+        return ((float) 2 / ( 1 + (float) Math.exp(-result)) - (float) 1);
+    }
+
+    private int getIntersectionSize (int[] A, int[] B) {
+        int i = 0, j = 0;
+        int result = 0;
+        
+        while (i < A.length && j < B.length) {
+            if (A[i] == B[j]) {
+                result++;
+                i++;
+                j++;
+            } else if (A[i] > B[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return result == 0 ? 1 : result;
+    }
+
+}
