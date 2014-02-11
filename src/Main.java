fc87efdc3e8db2ec21c4cf6e5899a5a6d2d27dbf
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-02-11 14:16:07 +0100

    Add baseline measure.

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,16 @@ public class Main {
         TagmeConfig.init("/home/irproject/config.xml");
         String groupName = "group3";
         String groupPw = "7dGHlSa";
+
+        // RelatednessMeasure rel = RelatednessMeasure.create("en");
         RelatednessMeasure rel = new Probability("en");
 
         // Comment the following line to run the program in production mode.
-        // IRProjectExperiments.dumpRelatednessExperiment(groupName, rel);
+        IRProjectExperiments.dumpRelatednessExperiment(groupName, rel);
 
         // Comment the following two lines to run the program in testing mode.
-        IRProjectExperiments.launchTagMeExperiment(groupName, groupPw, rel);
-        IRProjectExperiments.launchRelatednessExperiment(groupName, groupPw, rel);
+        // IRProjectExperiments.launchTagMeExperiment(groupName, groupPw, rel);
+        // IRProjectExperiments.launchRelatednessExperiment(groupName, groupPw, rel);
     }
 
 }
